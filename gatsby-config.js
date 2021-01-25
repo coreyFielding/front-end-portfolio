94849ea2761d58ebabe27953e2a2a10712336aa3
Corey Joshua Fielding <coreyfielding@coreys-mbp.home>
2021-01-25 13:41:31 +0000

    chore(project): Remove boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "8p7swc8i",
+        dataset: "production"
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
